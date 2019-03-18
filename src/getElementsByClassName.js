85edab5ade43c1fc8e49df5bc4806992febdd792
Daniel Sangyoon Kim <ksydaniel@gmail.com>
2019-03-19 01:39:42 +0900

    finish stringifyJSON.js and getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// var getElementsByClassName = function(className) {
+//   let newArr = [];
+//
+//   function recursiveFunc(el) {
+//     for (let i = 0; i < el.length; i++) {
+//       if (el[i].className.split(" ").includes(className)) {
+//         newArr.push(el[i]);
+//       }
+//       if (el[i].children) {
+//         recursiveFunc(el[i].children);
+//       }
+//     }
+//   }
+//   recursiveFunc(document.body.children);
+//   return newArr;
+// };
+
+var getElementsByClassName = function(className) {
+  let newArr = [];
+  function recursiveFunc(el) {
+    if (el.classList !== undefined && el.classList.contains(className)) {
+      newArr.push(el);
+    }
+    if (el.childNodes) {
+      el.childNodes.forEach(childNode => recursiveFunc(childNode));
+    }
+  }
+  recursiveFunc(document.body);
+  return newArr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null || obj === Infinity || obj === NaN) {
+    return "null";
+  }
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return obj.toString();
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else if (typeof obj === "function" || typeof obj === undefined) {
+    return undefined;
+  }
+
+  if (Array.isArray(obj)) {
+    let newArr = [];
+    if (!obj.length) {
+      return "[]";
+    } else if (obj.length) {
+      obj.forEach(i => {
+        newArr.push(stringifyJSON(i));
+      });
+      return "[" + newArr.join() + "]";
+    }
+  }
+
+  if (typeof obj === "object") {
+    let newArr = [];
+    for (let key in obj) {
+      if (typeof obj[key] === "function" || obj[key] === undefined) {
+        continue;
+      } else {
+        newArr.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    }
+    return "{" + newArr.join() + "}";
+  }
 };
